651371fae17d022a3aacd6d6d876d00ea460f7df
Thibault Prat-Hagler <thibault.hagler@gmail.com>
2014-01-25 00:26:00 +0100

    Rails 4 init

---
+++ Walla/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Walla::Application.config.secret_key_base = 'e9353be5aec700147b1e00fabb949c05abb840398b2daea9c95885b0eba9573e12714de3743f018b3a7b9a406f3076cae243114cc26af599bc9eddecffee7cc9'

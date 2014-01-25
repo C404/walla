f3f809aee20f9116891839c0aa045f6e8dd33cf6
Julien 'Lta' BALLET <contact@lta.io>
2014-01-25 12:36:20 +0100

    Axa Document ES

--- Walla/config/routes.rb
+++ Walla/config/routes.rb
@@ -1,6 +1,7 @@
 Walla::Application.routes.draw do
   get "framer/index"
   resources :tweets
+  resources :axa, only: [:create, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

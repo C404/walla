69a05a58488e571c58a4a013cba012a9a58b2f42
Julien 'Lta' BALLET <contact@lta.io>
2014-01-25 04:25:20 +0100

    The bot answers !

--- Walla/lib/daemons/twitter_bot.rb
+++ Walla/lib/daemons/twitter_bot.rb
@@ -28,17 +28,34 @@ stream = Twitter::Streaming::Client.new do |config|
   config.access_token_secret  = ENV['TWITTER_ACCESS_SECRET']
 end
 
+api = Faraday.new(:url => 'http://localhost:3000') do |faraday|
+  faraday.request  :url_encoded             # form-encode POST params
+  faraday.response :logger                  # log requests to STDOUT
+  faraday.adapter Faraday.default_adapter   # make requests with Net::HTTP
+  #faraday.adapter :rack
+end
+
+me = client.user(skip_status: true)
 
 while($running) do
 
+  puts "Listening to stream..."
   stream.user do |object|
     case object
     when Twitter::Tweet
-      puts "It's a tweet!"
-      puts "--> #{object.text}"
+      unless object.user.screen_name == me.screen_name
+        api.post '/tweets.json', tweet: {
+          customer_account: object.user.screen_name,
+          customer_msg:     object.text,
+          msg_url:          object.url
+        }
+        client.update("@#{object.user.screen_name} Thank you for tweeting me #{rand 999999}",
+          in_reply_to_status: object)
+      end
     when Twitter::DirectMessage
       puts "It's a direct message!"
       puts "--> #{object.text}"
+      puts object.sender.screen_name
     when Twitter::Streaming::StallWarning
       warn "Falling behind!"
     else

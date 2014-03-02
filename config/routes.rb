98d735610f0dd5195636bdad6ff08cae5f498b8a
enzo <enzo@enzo.enzo>
2014-03-02 18:01:04 +0100

    oauth

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Walla::Application.routes.draw do
   # Bullshit
   root 'doc#index'
   get  '/documentation', to: 'doc#documentation', as: :documentation
+
+
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks' }
 end

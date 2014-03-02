dcc16f9e55132d9ada71a40a9b9eeb4000c50491
enzo <enzo@enzo.enzo>
2014-03-02 18:47:59 +0100

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Walla::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+
+  config.use_ssl = true 
+  config.ssl_port = 3001
 end

50789967961680f41012d048e807df180b8aa741
Julien 'Lta' BALLET <contact@lta.io>
2014-03-07 23:42:32 +0100

    Caca

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140305130129) do
+ActiveRecord::Schema.define(version: 20140307223958) do
 
   create_table "auto_responders", force: true do |t|
     t.string   "matcher"
